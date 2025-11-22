8a1a96501990afd9bfc6c2ca5a85637835557348
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-22 17:52:34 +0300

    Help HELP FUCKING LLVM

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -5,7 +5,7 @@ wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip 
 unzip android-ndk-r10e-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
-./waf configure -T release --togles --android=armeabi-v7a-hard,host,21 --disable-warns &&
+./waf configure -T release --android=armeabi-v7a-hard,host,21 --togles --disable-warns &&
 ./waf build
 cd build
 zip -r ../build.zip .
