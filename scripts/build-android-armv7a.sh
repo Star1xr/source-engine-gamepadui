d48210bd5d38409202db60db156eae63533ae223
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-24 18:39:10 +0300

    K

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -5,7 +5,9 @@ wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip 
 unzip android-ndk-r10e-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
-chmod +x android-ndk-r10e/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip
+export STRIP=strip
+export RANLIB=ranlib
+export AR=ar
 ./waf configure -T release --android=armeabi-v7a-hard,host,21 --togles --disable-warns &&
 ./waf build
 cd build
