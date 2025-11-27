b474ceca87dd88d9eaa964a8bd9f2d616e8a5062
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-28 02:29:54 +0300

    K

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -22,6 +22,8 @@ NDK_COMPAT_NAME="android-ndk-r10e"
 wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
 unzip android-ndk-r10e-linux-x86_64.zip
 
+apt install clang
+
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e
 export NDK_HOME=$PWD/android-ndk-r10e
 export PATH=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
@@ -29,24 +31,7 @@ export PATH=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
 # 1. LLVM Path (Likely 'llvm-3.6' in r10e)
 # If Waf is looking for 'llvm-strip', it needs this path.
 NDK_LLVM_TOOLCHAIN_BIN="$NDK_HOME/toolchains/llvm-3.6/prebuilt/linux-x86_64/bin"
-
-
-# --- FIX 2 & 3: Define, Export Path, and Force Compiler Variables ---
-# The NDK r10e's most stable toolchain for armeabi-v7a is GCC 4.9.
-NDK_GNU_TOOLCHAIN_BIN="$NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
-
-# 2. Add the toolchain bin directory to PATH so utilities like 'strip' are found.
-# down there
-
-# 3. Explicitly set CC and CXX to the full path of the GNU compilers.
-# This forces Waf to use the specific compiler for its initial check.
-export CC="$NDK_GNU_TOOLCHAIN_BIN/arm-linux-androideabi-gcc"
-export CXX="$NDK_GNU_TOOLCHAIN_BIN/arm-linux-androideabi-g++"
-
-
-
-# Export the PATH, prioritizing the LLVM path first to resolve the 'llvm-strip' issue.
-export PATH="$NDK_LLVM_TOOLCHAIN_BIN:$NDK_GNU_TOOLCHAIN_BIN:$PATH"
+export PATH="$NDK_LLVM_TOOLCHAIN_BIN:$PATH"
 
 
 ./waf configure -T release --android=armeabi-v7a-hard,host,21 --togles --disable-warns &&
