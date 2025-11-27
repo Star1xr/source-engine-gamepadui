197de865f317e3df3cc2921fbe5f887188b7d827
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-27 22:29:15 +0300

    This is gonna work now or im gonna be bay harbour butcher soon

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 git submodule init && git submodule update
-wget https://dl.google.com/android/repository/android-ndk-r20b-linux-x86_64.zip -o /dev/null
-unzip android-ndk-r20b-linux-x86_64.zip
-export ANDROID_NDK_HOME=$PWD/android-ndk-r20b
-export NDK_HOME=$PWD/android-ndk-r20b
+wget https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip -o /dev/null
+unzip android-ndk-r17c-linux-x86_64.zip
+export ANDROID_NDK_HOME=$PWD/android-ndk-r17c
+export NDK_HOME=$PWD/android-ndk-r17c
 export PATH=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
 ./waf configure -T release --android=armeabi-v7a,host,21 --togles --disable-warns &&
 ./waf build
