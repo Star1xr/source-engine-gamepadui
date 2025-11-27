66124b81625a4c126e39071099de2c9f0b027232
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-27 23:23:06 +0300

    Kkk

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -15,18 +15,18 @@ set -e
 
 git submodule init && git submodule update
 
-NDK_VERSION="r17c"
+NDK_VERSION="r10c"
 NDK_UNZIPPED_NAME="android-ndk-${NDK_VERSION}"
 NDK_COMPAT_NAME="android-ndk-r10e"
 
 wget https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux-x86_64.zip -O android-ndk-${NDK_VERSION}-linux-x86_64.zip
 unzip -q android-ndk-${NDK_VERSION}-linux-x86_64.zip
 
-# Patch NDK version to 10.3.0 to bypass waf's 'Unknown NDK revision: 17' error.
-sed -i 's/Pkg.Revision=.*/Pkg.Revision=10.3.0/g' "$PWD/${NDK_UNZIPPED_NAME}/source.properties"
-
 # Fix 'llvm strip is not executable' error by ensuring permissions are set.
 chmod -R +x "$PWD/${NDK_UNZIPPED_NAME}/toolchains/llvm/prebuilt/linux-x86_64/bin"
+# ALSO ensure permissions are set for the critical GNU toolchain binaries
+chmod -R +x "$PWD/${NDK_UNZIPPED_NAME}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
+
 
 # Rename the NDK directory to r10e for path compatibility.
 mv $PWD/${NDK_UNZIPPED_NAME} $PWD/${NDK_COMPAT_NAME}
@@ -34,9 +34,15 @@ mv $PWD/${NDK_UNZIPPED_NAME} $PWD/${NDK_COMPAT_NAME}
 export ANDROID_NDK_HOME=$PWD/$NDK_COMPAT_NAME
 export NDK_HOME=$PWD/$NDK_COMPAT_NAME
 export NDK="$NDK_HOME"
-export PATH="$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
 
-./waf configure -T release --android=armeabi-v7a,host,21 --togles --disable-warns &&
+# --- CRITICAL PATH CORRECTION ---
+# This NDK relies on the specific GNU toolchain path for many prefixed binaries (like 'strip').
+NDK_GNU_TOOLCHAIN_BIN="$NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
+
+# We include both the modern LLVM path and the required older GNU path in the PATH variable.
+export PATH="$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$NDK_GNU_TOOLCHAIN_BIN:$PATH"
+
+./waf configure -T release --android=armeabi-v7a,hard,host,21 --togles --disable-warns &&
 ./waf build
 
 cd build
