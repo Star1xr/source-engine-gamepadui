4ab96ba3900e5b350374b7845378b06dc1d4d12d
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-27 22:05:55 +0300

    Yea obviusvuscus

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -2,7 +2,7 @@
 
 git submodule init && git submodule update
 wget https://dl.google.com/android/repository/android-ndk-r25c-linux.zip -o /dev/null
-unzip android-ndk-r29-linux.zip
+unzip android-ndk-r25c-linux.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r25c
 export NDK_HOME=$PWD/android-ndk-r25c
 export PATH=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
