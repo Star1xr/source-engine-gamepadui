c642709c36f8f63fd1787d587dcc6a580315b493
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-27 23:13:46 +0300

    K

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 git submodule init && git submodule update
-wget https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip -o /dev/null
-unzip android-ndk-r17c-linux-x86_64.zip
-mv $PWD/android-ndk-r17c $PWD/android-ndk-r10e
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
+unzip android-ndk-r10e-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e
-
-# This command changes the revision number inside the NDK's source.properties file
-# from its original value (e.g., 17.2.4988785) to '10.3.0', which your waf script should recognize.
-sed -i 's/Pkg.Revision=.*/Pkg.Revision=10.3.0/g' "$PWD/android-ndk-r10e/source.properties"
-
 export NDK_HOME=$PWD/android-ndk-r10e
 export PATH=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
 ./waf configure -T release --android=armeabi-v7a,host,21 --togles --disable-warns &&
