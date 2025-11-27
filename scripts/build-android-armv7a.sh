e279bec2c2dab430d723294e423ba234e9f74217
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-27 22:03:58 +0300

    Gotta crashout

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 git submodule init && git submodule update
-wget https://dl.google.com/android/repository/android-ndk-r29-linux.zip -o /dev/null
+wget https://dl.google.com/android/repository/android-ndk-r25c-linux.zip -o /dev/null
 unzip android-ndk-r29-linux.zip
-export ANDROID_NDK_HOME=$PWD/android-ndk-r29
-export NDK_HOME=$PWD/android-ndk-r29
+export ANDROID_NDK_HOME=$PWD/android-ndk-r25c
+export NDK_HOME=$PWD/android-ndk-r25c
 export PATH=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
 ./waf configure -T release --android=armeabi-v7a-hard,host,21 --togles --disable-warns &&
 ./waf build
