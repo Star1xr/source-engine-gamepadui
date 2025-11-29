a6e5563c33933573dc7bc79a60e4370daa916b8e
Star1xr <166748405+Star1xr@users.noreply.github.com>
2025-11-29 17:18:07 +0300

    gggsggsggFUck
    Added commands to update package list and install dependencies.

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -6,6 +6,8 @@ unzip android-ndk-r10e-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e
 export NDK_HOME=$PWD/android-ndk-r10e
 export PATH=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+sudo apt-get update
+sudo apt-get install libatomic1 libgcc-s1 libstdc++6 gcc-multilib g++-multilib ninja-build libfontconfig-dev
 ./waf configure -T release --android=armeabi-v7a,host,21 --togles --disable-warns &&
 ./waf build
 cd build
